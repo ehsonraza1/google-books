4272e11afd9af709d414ad4b94346e8ad62b6d3f
Ehson Raza <ehson.raza1@outlook.com>
2020-09-14 22:20:09 -0500

    Added API routes for front end

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const routes = require("./client/public/routes");
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // Define API routes here
-
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
